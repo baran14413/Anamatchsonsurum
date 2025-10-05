9b7764bb0796ec0cc687b0ecbe77956d690492db
Wilmar Nickel <baranguler.65091@gmail.com>
2025-10-05 13:17:14 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

---
+++ src/app/profilini-tamamla/page.tsx
@@ -0,0 +1,8 @@
+
+'use client';
+
+import ProfileCompletionForm from "@/components/profile-completion-form";
+
+export default function ProfileCompletionPage() {
+  return <ProfileCompletionForm />;
+}
