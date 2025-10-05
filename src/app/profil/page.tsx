85c4bfd979c67f28cf55ec800b32802f940ac043
Wilmar Nickel <baranguler.65091@gmail.com>
2025-10-05 20:14:45 +0000

    Try fixing this error: `Runtime Error: Error: ChevronRight is not define

--- src/app/profil/page.tsx
+++ src/app/profil/page.tsx
@@ -10,7 +10,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog';
-import { Flame, Star, Zap, ShieldCheckIcon, GalleryHorizontal } from 'lucide-react';
+import { Flame, Star, Zap, ShieldCheckIcon, GalleryHorizontal, ChevronRight } from 'lucide-react';
 import { langTr } from '@/languages/tr';
 import { useToast } from '@/hooks/use-toast';
 import { Icons } from '@/components/icons';
