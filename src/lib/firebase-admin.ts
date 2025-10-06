4983dcd15befbeec8ad3350a73fa8239cfe4b061
Wilmar Nickel <baranguler.65091@gmail.com>
2025-10-06 09:41:28 +0000

    Buseferde boyle diyor

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,13 +1,16 @@
-import { initializeApp, getApps, App } from 'firebase-admin/app';
+
+import { initializeApp, getApps, App, applicationDefault } from 'firebase-admin/app';
 import { getFirestore, Firestore } from 'firebase-admin/firestore';
 
 let adminApp: App;
 let db: Firestore;
 
 if (!getApps().length) {
-  adminApp = initializeApp();
+    adminApp = initializeApp({
+        credential: applicationDefault(),
+    });
 } else {
-  adminApp = getApps()[0]!;
+    adminApp = getApps()[0]!;
 }
 
 db = getFirestore(adminApp);
