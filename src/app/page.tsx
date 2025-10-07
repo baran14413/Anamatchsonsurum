786062d5c9e0a8d25816df8f128b47ce1dceb0cc
Wilmar Nickel <baranguler.65091@gmail.com>
2025-10-07 15:34:59 +0000

    Bazi uzun ekranlarda google ile giris yap alta kayiyiyor indexdeki

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,9 +89,9 @@ export default function WelcomePage() {
   }
 
   return (
-    <div className="flex min-h-screen flex-col bg-gradient-to-b from-blue-500 to-purple-600 text-white">
+    <div className="flex min-h-dvh flex-col bg-gradient-to-b from-blue-500 to-purple-600 text-white">
       <main className="flex flex-1 flex-col items-center p-8 text-center">
-        <div className="flex-1 flex flex-col justify-center items-center">
+        <div className="flex flex-1 flex-col justify-center items-center">
           <motion.div
             className="transform-gpu"
             animate={{
