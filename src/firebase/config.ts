3a0517ee1f24924a7c96ae27b9ddbb2f3ccc04ca
Wilmar Nickel <baranguler.65091@gmail.com>
2025-10-18 19:21:23 +0000

    Firebasw storageyi bematch-new.firebasestorage.app olarak gunxellenenvde

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyBTYHLORHg_HuLzAg74XCanqLE82e92NJI",
   authDomain: "bematch-new.firebaseapp.com",
   projectId: "bematch-new",
-  storageBucket: "bematch-new.appspot.com",
+  storageBucket: "bematch-new.firebasestorage.app",
   messagingSenderId: "108504362423",
   appId: "1:108504362423:web:3781383a547a09590d8c32",
   measurementId: "G-P6LCQY34GB"
