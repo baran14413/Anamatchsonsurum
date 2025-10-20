a63050dca7e665102e164311dabd5cf3297364fc
Wilmar Nickel <baranguler.65091@gmail.com>
2025-10-20 13:03:03 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -25,6 +25,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Input } from '@/components/ui/input';
 import { cn } from '@/lib/utils';
