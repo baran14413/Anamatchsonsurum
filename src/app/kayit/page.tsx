6443678badfc6b92a76b501b3d2fc80299ae2672
Wilmar Nickel <baranguler.65091@gmail.com>
2025-10-21 10:50:48 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/app/kayit/page.tsx
+++ src/app/kayit/page.tsx
@@ -1,6 +1,8 @@
+
 'use client';
 
 import { useState, useEffect } from "react";
+import * as React from 'react';
 import { useForm, Controller } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
