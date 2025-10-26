4b3d48330b1408967e2290cf7a36ce16c00c0320
Wilmar Nickel <baranguler.65091@gmail.com>
2025-10-26 13:39:52 +0000

    Dostum logomuz logo.png /img icinde

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -5,7 +5,7 @@ import beGoldLogo from '@/img/begold.png';
 export const Icons = {
   logo: (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
     // eslint-disable-next-line @next/next/no-img-element
-    <img src="/logo.png" alt="BeMatch Logo" {...props} />
+    <img src="/img/logo.png" alt="BeMatch Logo" {...props} />
   ),
   adminLogo: (props: SVGProps<SVGSVGElement>) => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" {...props}>
