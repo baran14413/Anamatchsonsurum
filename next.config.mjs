9e3d6d7621dd37775895ed4ee9da26f0f8f8f345
Wilmar Nickel <baranguler.65091@gmail.com>
2025-10-29 16:05:46 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    transpilePackages: ['lamejs'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    // This is to prevent 'fs' module not found error in client-side code
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
