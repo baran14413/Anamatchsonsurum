714585f713d9e175bc9c2c45d2d0a4c06957bb8d
Wilmar Nickel <baranguler.65091@gmail.com>
2025-11-11 16:15:10 +0000

    Try fixing this error: `Unhandled Runtime Error: FirebaseError: Firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "bematch-9ekim-yedek-2351-cfa71",
   "appId": "1:558355743951:web:107571f68fade13e4063ba",
+  "storageBucket": "bematch-9ekim-yedek-2351-cfa71.appspot.com",
   "apiKey": "AIzaSyBGe7auUclq8vZ1kzNGHtQo_QDMMPnzVyk",
   "authDomain": "bematch-9ekim-yedek-2351-cfa71.firebaseapp.com",
   "measurementId": "",
